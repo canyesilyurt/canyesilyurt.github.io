f91d701c1994f212981ba90228fc62ca09607463
Can Yesilyurt <36952967+canyesilyurt@users.noreply.github.com>
2024-07-22 00:08:50 +0300

    ‘

--- assets/static/chunks/app/page-2cea072c2e337c49.js
+++ assets/static/chunks/app/page-2cea072c2e337c49.js
@@ -433,6 +433,18 @@
                                                                         }),
                                                                     ]);
                                                                     break;
+                                                                    case "lovu":
+                                                                    o([
+                                                                        ...l,
+                                                                        (0, s.jsx)(k.bs, {
+                                                                            children: (0, s.jsx)("div", {
+                                                                                className: " text-wrap text-primary",
+                                                                                children:
+                                                                                    "lovusila",
+                                                                            }),
+                                                                        }),
+                                                                    ]);
+                                                                    break;
                                                                 case "ls":
                                                                     o([...l, (0, s.jsx)(k.bs, { children: "What is your point man?" })]);
                                                                     break;
